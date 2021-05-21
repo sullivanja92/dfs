af1ac48309e2765ecd0bfd720cdf5bda51f9531d
Josh Sullivan <joshsullivan@Joshs-MacBook-Air.local>
2021-05-21 10:35:22 -0500

    Refactor pulp utils module and update unit tests.

--- dfs/pulp_utils.py
+++ dfs/pulp_utils.py
@@ -1,15 +1,13 @@
 from pulp import LpProblem, LpStatus
 
 
-def int_index_from_lp_variable_name(name: str, delimiter: str = '_') -> int:
-    if name is None or name == '':
-        raise ValueError('Name must not be None or empty')
-    if delimiter is None or delimiter not in name:
-        raise ValueError('The delimiter must be found in the variable name')
-    return int(name.split(delimiter)[1])
-
-
 def is_optimal_solution_found(problem: LpProblem) -> bool:
+    """
+    Returns a bool indicating whether an optimal solution has been found for the given problem.
+
+    :param problem: the LpProblem variable
+    :return: True if an optimal solution is found, else False
+    """
     if problem is None:
         raise ValueError('Problem cannot be none')
     return LpStatus[problem.status] == 'Optimal'
